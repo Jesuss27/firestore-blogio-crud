caf161a0018e93a279aeaf1ef68aba3957a1a08c
Jesus <65581378+Jesuss27@users.noreply.github.com>
2021-05-29 11:43:23 -0700

    List-Post TS logic

--- src/app/list-post/list-post.component.ts
+++ src/app/list-post/list-post.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from "../post.model";
+import { PostService } from "../post.service";
+
 
 @Component({
   selector: 'app-list-post',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-post.component.css']
 })
 export class ListPostComponent implements OnInit {
+  Posts : Post[] ;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(private postService : PostService ) { }
+
+  ngOnInit() {
+    this.postService.getPostList().subscribe(res => {
+      this.Posts = res.map(e => {
+        return {
+          id: e.payload.doc.id,
+          ...e.payload.doc.data() as { }
+        } as Post
+
+      })
+    });
   }
+
+  removePost = post => this.postService.deletePost(post) ;
 
 }
