21d43d7fcb5a026f63b73bde89c500c9af44fa08
Jesus <65581378+Jesuss27@users.noreply.github.com>
2021-05-28 20:44:46 -0700

    Create Routes with routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CreatePostComponent } from "./create-post/create-post.component"
+import { EditPostComponent } from "./edit-post/edit-post.component"
+import { ListPostComponent } from "./list-post/list-post.component"
+
+const routes: Routes = [
+  { path: "", redirectTo:"/create" , pathMatch:"full"},
+  { path: "create" , component: CreatePostComponent },
+  { path: "update-post/:id" , component: EditPostComponent },
+  { path: "list-posts" , component: ListPostComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
