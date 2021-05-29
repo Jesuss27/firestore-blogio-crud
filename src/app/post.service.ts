1a5f9eabb8400ad3039655eb70dc6240cde6a25d
Jesus <65581378+Jesuss27@users.noreply.github.com>
2021-05-28 20:37:30 -0700

    Define Post Service Methods

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from "@angular/fire/firestore" ; 
+import { Post } from "../app/post.model" ; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  constructor(private angularFirestore : AngularFirestore ) { }
+
+
+  getPostDoc(id) {
+    return this.angularFirestore // 
+    .collection("post-collection")
+    .doc(id)
+    .valueChanges()
+  }
+
+  getPostList() {
+    return this.angularFirestore
+    .collection("post-collection")
+    .snapshotChanges();
+  }
+
+  createPost( post:Post ) {
+    return new Promise<any>((resolve, reject) => {
+      this.angularFirestore
+      .collection("post-collection")
+      .add(post)
+      .then(response => {console.log(response)} , error => reject(error))
+
+    } );
+  }
+
+  deletePost(post) {
+    return this.angularFirestore
+    .collection("post-collection")
+    .doc(post.id)
+    .delete();
+  }
+
+  updatePost(post: Post, id ) {
+    return this.angularFirestore
+    .collection("post-collection")
+    .doc(post.id)
+    .update({
+      title: post.title,
+      date: post.date,
+      notes: post.notes,
+    })
+  }
+
 }
