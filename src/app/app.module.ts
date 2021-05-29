9994c7fd7f3b6bd6fc4d12bac097982b73e57676
Jesus <65581378+Jesuss27@users.noreply.github.com>
2021-05-28 19:32:55 -0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { ListPostComponent } from './list-post/list-post.component';
+import { EditPostComponent } from './edit-post/edit-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatePostComponent,
+    ListPostComponent,
+    EditPostComponent
   ],
   imports: [
     BrowserModule,
